8ea7a55e5eebd9439e9b052f7a4dd05360ea39f0
PowerPlayers2023 <thepowerplayers2023@gmail.com>
2023-02-14 13:18:56 +0100

    First commit

---
+++ src/main/java/esprit/tn/pidevthepowerplayers/PiDevThePowerPlayersApplication.java
@@ -0,0 +1,13 @@
+package esprit.tn.pidevthepowerplayers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiDevThePowerPlayersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PiDevThePowerPlayersApplication.class, args);
+    }
+
+}
